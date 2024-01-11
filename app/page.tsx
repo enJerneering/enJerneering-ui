80387c8e4b64bbfdf0a675a0af7441156613097d
Hung Truong <hungit2211@gmail.com>
2024-01-11 21:56:04 +0700

    Create contact components

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,12 @@ import {
   ctaData,
   teamSectionType,
   teamSectionData,
+  contactType,
+  contactData,
 } from "./pageData";
 import CallToAction from "./_components/CallToAction";
 import TeamSection from "./_components/TeamSection";
+import Contact from "./_components/Contact";
 
 export const metadata: Metadata = {
   title: seo.title,
@@ -28,6 +31,7 @@ export default function Home() {
       <Header type={headerType} data={headerData} />
       <CallToAction type={ctaType} data={ctaData} />
       <TeamSection type={teamSectionType} data={teamSectionData} />
+      <Contact type={contactType} data={contactData} />
     </div>
   );
 }
