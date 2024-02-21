f4a7265df9c20dac9c946347c189b3df8ba3721b
Hung Truong <hungit2211@gmail.com>
2024-02-21 20:45:14 +0700

    Update main content components

--- app/_components/MainContent/_MainContentType4.tsx
+++ app/_components/MainContent/_MainContentType4.tsx
@@ -2,62 +2,47 @@
 "use client";
 
 import React from "react";
-import { MainContentData, SubHeading } from "./types/MainContentData";
-import Image from "next/image";
+import { MainContentData } from "./types/MainContentData";
+import ButtonActions from "../ButtonActions";
 
 interface MainContentProps {
   data: MainContentData;
 }
 
-const SubHeadingItem = ({ item }: { item: SubHeading }): JSX.Element => (
-  <div className="flex flex-col p-2 gap-3.5 items-center">
-    <Image
-      src="/img/check-circle.svg"
-      width={24}
-      height={24}
-      alt="check-icon"
-    />
-    <div className="flex flex-col items-center text-center gap-1">
-      <span className="text-sm font-semibold text-neutral-800">
-        {item.title}
-      </span>
-      <span className="text-sm font-normal text-neutral-500">
-        {item.subtitle}
-      </span>
-    </div>
-  </div>
-);
-
 const MainContentType4: React.FC<MainContentProps> = ({ data }) => {
   const {
-    tagLine = "Default TagLine",
     title = "Default Title",
     subtitle = "Default Subtitle",
-    subHeading = [],
+    tagLine = "Default Tagline",
+    imgUrl = "default-image.jpg",
+    primaryLabel = "Primary Button",
+    secondaryLabel = "Secondary Button",
+    primaryUrl = "#",
+    secondaryUrl = "#",
   } = data;
 
   return (
     <div className="bg-gray-50">
       <div className="mx-auto max-w-[1440px] px-6 py-20 lg:p-20">
-        <div className="mx-auto w-full flex flex-col gap-6 lg:mx-0 text-center">
-          <span className="text-base uppercase font-bold text-neutral-500">
-            {tagLine}
-          </span>
-          <h2 className="text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl">
-            {title}
-          </h2>
-          <p className="text-base text-neutral-500">{subtitle}</p>
+        <div className="flex flex-col items-center gap-10">
+          <div className="flex flex-col gap-6 text-center max-w-[800px]">
+            <span className="text-base uppercase font-bold text-neutral-500">
+              {tagLine}
+            </span>
+            <h5>{title}</h5>
+            <p className="text-base font-normal text-neutral-500">{subtitle}</p>
+          </div>
+          <ButtonActions
+            primaryLabel={primaryLabel}
+            secondaryLabel={secondaryLabel}
+            primaryUrl={primaryUrl}
+            secondaryUrl={secondaryUrl}
+          />
         </div>
-        <div className="flex justify-center w-full mt-10">
-          <ul
-            role="list"
-            className="mx-auto mt-10 grid max-w-screen-xl grid-cols-1 gap-x-10 gap-y-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none xl:grid-cols-3"
-          >
-            {subHeading.map((item, index) => (
-              <SubHeadingItem item={item} key={index} />
-            ))}
-          </ul>
-        </div>
+        <div
+          className="flex justify-center w-full h-[600px] mt-10 lg:mt-20 bg-center bg-cover rounded-2xl shadow-xl"
+          style={{ backgroundImage: `url(${imgUrl})` }}
+        ></div>
       </div>
     </div>
   );
